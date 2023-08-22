5941c1ca952986ab4332b8284186ac7106536818
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2023-08-22 10:21:36 -0300

    add linter and fix files

--- test/config.js
+++ test/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const Config = {
-    AMPLIA_ENDPOINT: process.env.AMPLIA_ENDPOINT,
-    AMPLIA_API_KEY: process.env.AMPLIA_API_KEY,
-    AMPLIA_CA_ID: process.env.AMPLIA_CA_ID,
-}
+	AMPLIA_ENDPOINT: process.env.AMPLIA_ENDPOINT,
+	AMPLIA_API_KEY: process.env.AMPLIA_API_KEY,
+	AMPLIA_CA_ID: process.env.AMPLIA_CA_ID,
+};
 
 exports.Config = Config;
--- test/test-cases/orders.test.js
+++ test/test-cases/orders.test.js
@@ -1,258 +1,258 @@
-const { faker } = require("@faker-js/faker");
+const { faker } = require('@faker-js/faker');
 
 
 const {
-    CertificateKinds,
-    OrderStatus,
-    CertificateFormats,
-} = require("../../lib/enums");
-const { AmpliaClient } = require("../../lib/amplia-client");
-const { CreateOrderRequest } = require("../../lib/create-order-request");
-const generators = require("../generators");
-const { Config } = require("../config");
-const { skipWhenNotConfigured } = require("../case");
-const { KeyGenerator } = require("pki-express");
+	CertificateKinds,
+	OrderStatus,
+	CertificateFormats,
+} = require('../../lib/enums');
+const { AmpliaClient } = require('../../lib/amplia-client');
+const { CreateOrderRequest } = require('../../lib/create-order-request');
+const generators = require('../generators');
+const { Config } = require('../config');
+const { skipWhenNotConfigured } = require('../case');
+const { KeyGenerator } = require('pki-express');
 const {
-    pkiBrazilCertificateParameters,
-    pkiParaguayCertificateParameters,
-    sslCertificateParameters,
-    cnbCertificateParameters,
-    cieCertificateParameters,
-    arispCertificateParameters
-} = require("../constants");
+	pkiBrazilCertificateParameters,
+	pkiParaguayCertificateParameters,
+	sslCertificateParameters,
+	cnbCertificateParameters,
+	cieCertificateParameters,
+	arispCertificateParameters
+} = require('../constants');
 
-const LONG_TIMEOUT = "30000"; // 30 seconds
+const LONG_TIMEOUT = '30000'; // 30 seconds
 
 const ampliaClient = new AmpliaClient({
-    endpoint: Config.AMPLIA_ENDPOINT,
-    apiKey: Config.AMPLIA_API_KEY,
+	endpoint: Config.AMPLIA_ENDPOINT,
+	apiKey: Config.AMPLIA_API_KEY,
 });
 
 const getCertificateParams = (certificateFormat, user) => {
-    switch (certificateFormat) {
-        case CertificateFormats.PKI_BRAZIL:
-            return pkiBrazilCertificateParameters(user);
-        case CertificateFormats.PKI_PARAGUAY:
-            return pkiParaguayCertificateParameters(user);
-        case CertificateFormats.SSL:
-            return sslCertificateParameters(user);
-        case CertificateFormats.CNB:
-            return cnbCertificateParameters(user);
-        case CertificateFormats.CIE:
-            return cieCertificateParameters(user);
-        case CertificateFormats.ARISP:
-            return arispCertificateParameters(user);
-        default:
-            throw `Certificate "format" field not supported on model for CertificateParameters: ${format}`;
-    }
+	switch (certificateFormat) {
+	case CertificateFormats.PKI_BRAZIL:
+		return pkiBrazilCertificateParameters(user);
+	case CertificateFormats.PKI_PARAGUAY:
+		return pkiParaguayCertificateParameters(user);
+	case CertificateFormats.SSL:
+		return sslCertificateParameters(user);
+	case CertificateFormats.CNB:
+		return cnbCertificateParameters(user);
+	case CertificateFormats.CIE:
+		return cieCertificateParameters(user);
+	case CertificateFormats.ARISP:
+		return arispCertificateParameters(user);
+	default:
+		throw `Certificate "format" field not supported on model for CertificateParameters: ${certificateFormat}`;
+	}
 };
 
 function mountOrderRequest(
-    hasPresetUser = false,
-    certificateFormat = CertificateFormats.PKI_BRAZIL
+	hasPresetUser = false,
+	certificateFormat = CertificateFormats.PKI_BRAZIL
 ) {
 
-    const user = hasPresetUser
-        ? {
-              name: "Pierre de Fermat",
-              cpf: "673.644.483-73", //CPF Falso de Pierre de Fermat
-          }
-        : {
-              name: faker.name.firstName,
-              cpf: "264.415.360-30", //CPF Falso
-          };
-          
-    // Create an order request.
-    const request = new CreateOrderRequest({
-        caId: Config.AMPLIA_CA_ID,
-        validityEnd: generators.generateDateTwoYearsFromNow(),
-        kind: CertificateKinds.PUBLIC_KEY,
-        parameters: getCertificateParams(certificateFormat, user),
-    });
+	const user = hasPresetUser
+		? {
+			name: 'Pierre de Fermat',
+			cpf: '673.644.483-73', //CPF Falso de Pierre de Fermat
+		}
+		: {
+			name: faker.name.firstName,
+			cpf: '264.415.360-30', //CPF Falso
+		};
 
-    return request;
+	// Create an order request.
+	const request = new CreateOrderRequest({
+		caId: Config.AMPLIA_CA_ID,
+		validityEnd: generators.generateDateTwoYearsFromNow(),
+		kind: CertificateKinds.PUBLIC_KEY,
+		parameters: getCertificateParams(certificateFormat, user),
+	});
+
+	return request;
 }
 
 const certificateFormats = [
-    [CertificateFormats.PKI_BRAZIL], 
-    [CertificateFormats.CNB], 
-    [CertificateFormats.SSL], 
-    [CertificateFormats.PKI_PARAGUAY], 
-    [CertificateFormats.CIE], 
-    [CertificateFormats.ARISP]
+	[CertificateFormats.PKI_BRAZIL],
+	[CertificateFormats.CNB],
+	[CertificateFormats.SSL],
+	[CertificateFormats.PKI_PARAGUAY],
+	[CertificateFormats.CIE],
+	[CertificateFormats.ARISP]
 ];
 
-describe.each(certificateFormats)("Test Cases for Amplia Node Client", () => {
-    // Test Case #1 for Amplia Node Client
-    skipWhenNotConfigured(
-        "Scenario 1: should be able to create, retrieve, and delete an order request with PKI Brazil Parameters",
-        async () => {
-            // Mount a default request using default PKIBrazilParameters
-            let request = mountOrderRequest(false);
+describe.each(certificateFormats)('Test Cases for Amplia Node Client', () => {
+	// Test Case #1 for Amplia Node Client
+	skipWhenNotConfigured(
+		'Scenario 1: should be able to create, retrieve, and delete an order request with PKI Brazil Parameters',
+		async () => {
+			// Mount a default request using default PKIBrazilParameters
+			let request = mountOrderRequest(false);
 
-            // Mount a default request using default PKIBrazilParameters
-            const order = await ampliaClient.createOrder(request);
-            const retrievedOrder = await ampliaClient.getOrder(order.id);
+			// Mount a default request using default PKIBrazilParameters
+			const order = await ampliaClient.createOrder(request);
+			const retrievedOrder = await ampliaClient.getOrder(order.id);
 
-            await ampliaClient.deleteOrder(order.id);
+			await ampliaClient.deleteOrder(order.id);
 
-            expect(retrievedOrder.id).toBe(order.id);
-            expect(retrievedOrder.caId).toBe(order.caId);
-            expect(retrievedOrder.kind).toBe(order.kind);
-            expect(retrievedOrder.alias).toBe(order.alias);
-            expect(retrievedOrder.validityEnd).toBe(order.validityEnd);
-            expect(retrievedOrder.name).toBe(order.name);
-            expect(retrievedOrder.certificateId).toBe(order.certificateId);
-            expect(retrievedOrder.status).toBe(order.status);
+			expect(retrievedOrder.id).toBe(order.id);
+			expect(retrievedOrder.caId).toBe(order.caId);
+			expect(retrievedOrder.kind).toBe(order.kind);
+			expect(retrievedOrder.alias).toBe(order.alias);
+			expect(retrievedOrder.validityEnd).toBe(order.validityEnd);
+			expect(retrievedOrder.name).toBe(order.name);
+			expect(retrievedOrder.certificateId).toBe(order.certificateId);
+			expect(retrievedOrder.status).toBe(order.status);
 
-            try {
-                // Try to get the same order, but it is deleted now
-                await ampliaClient.getOrder(order.id);
-            } catch (e) {
-                expect(e.name).toMatch("AmpliaError");
-                expect(e.code).toBe("OrderDeleted");
-            }
-        },
-        LONG_TIMEOUT
-    );
-    // end Test Case #1 for Amplia Node Client
+			try {
+				// Try to get the same order, but it is deleted now
+				await ampliaClient.getOrder(order.id);
+			} catch (e) {
+				expect(e.name).toMatch('AmpliaError');
+				expect(e.code).toBe('OrderDeleted');
+			}
+		},
+		LONG_TIMEOUT
+	);
+	// end Test Case #1 for Amplia Node Client
 
-    // Test Case #2 for Amplia Node Client
-    skipWhenNotConfigured(
-        "Scenario 2: should generate an order, retrieve, generate an order link and remove the order afterwards",
-        async () => {
-            // Mount a default request using default PKIBrazilParameters
-            let request = mountOrderRequest(false);
+	// Test Case #2 for Amplia Node Client
+	skipWhenNotConfigured(
+		'Scenario 2: should generate an order, retrieve, generate an order link and remove the order afterwards',
+		async () => {
+			// Mount a default request using default PKIBrazilParameters
+			let request = mountOrderRequest(false);
 
-            const order = await ampliaClient.createOrder(request);
-            // get the order issue link
-            const orderLink = await ampliaClient.getOrderIssueLink(order.id);
-            // Retrieve the order
-            const retrievedOrder = await ampliaClient.getOrder(order.id);
+			const order = await ampliaClient.createOrder(request);
+			// get the order issue link
+			const orderLink = await ampliaClient.getOrderIssueLink(order.id);
+			// Retrieve the order
+			const retrievedOrder = await ampliaClient.getOrder(order.id);
 
-            expect(retrievedOrder.id).toBe(order.id);
-            expect(retrievedOrder.caId).toBe(order.caId);
-            expect(retrievedOrder.kind).toBe(order.kind);
-            expect(retrievedOrder.alias).toBe(order.alias);
-            expect(retrievedOrder.validityEnd).toBe(order.validityEnd);
-            expect(retrievedOrder.name).toBe(order.name);
-            expect(retrievedOrder.certificateId).toBe(order.certificateId);
-            expect(retrievedOrder.status).toBe(order.status);
-            expect(orderLink).not.toBeNull();
-            expect(orderLink).toMatch(
-                new RegExp(
-                    "https://amplia.lacunasoftware.com/issue/[a-fA-F0-9]{32}"
-                )
-            );
+			expect(retrievedOrder.id).toBe(order.id);
+			expect(retrievedOrder.caId).toBe(order.caId);
+			expect(retrievedOrder.kind).toBe(order.kind);
+			expect(retrievedOrder.alias).toBe(order.alias);
+			expect(retrievedOrder.validityEnd).toBe(order.validityEnd);
+			expect(retrievedOrder.name).toBe(order.name);
+			expect(retrievedOrder.certificateId).toBe(order.certificateId);
+			expect(retrievedOrder.status).toBe(order.status);
+			expect(orderLink).not.toBeNull();
+			expect(orderLink).toMatch(
+				new RegExp(
+					'https://amplia.lacunasoftware.com/issue/[a-fA-F0-9]{32}'
+				)
+			);
 
-            // Delete the order after all tests
-            await ampliaClient.deleteOrder(order.id);
+			// Delete the order after all tests
+			await ampliaClient.deleteOrder(order.id);
 
-            try {
-                // Try to get the same order, but it is deleted now
-                await ampliaClient.getOrder(order.id);
-            } catch (e) {
-                expect(e.name).toMatch("AmpliaError");
-                expect(e.code).toBe("OrderDeleted");
-            }
-        },
-        LONG_TIMEOUT
-    );
-    // end Test Case #2 for Amplia Node Client
+			try {
+				// Try to get the same order, but it is deleted now
+				await ampliaClient.getOrder(order.id);
+			} catch (e) {
+				expect(e.name).toMatch('AmpliaError');
+				expect(e.code).toBe('OrderDeleted');
+			}
+		},
+		LONG_TIMEOUT
+	);
+	// end Test Case #2 for Amplia Node Client
 
-    // Test Case #3 for Amplia Node Client
-    skipWhenNotConfigured(
-        "Scenario 3: should generate an order, generate a key and a CSR using PkiExpress, then issue that certificate with its corresponding CSR",
-        async () => {
-            console.log(
-                "\nWARNING: This test requires PkiExpress, version greater than 1.11\n" +
-                    "Also, you should have the Pierre de Fermat certificate installed in your machine\n"
-            );
-            try {
-                // Mount a default request using default PKIBrazilParameters
-                let request = mountOrderRequest(true);
+	// Test Case #3 for Amplia Node Client
+	skipWhenNotConfigured(
+		'Scenario 3: should generate an order, generate a key and a CSR using PkiExpress, then issue that certificate with its corresponding CSR',
+		async () => {
+			console.log(
+				'\nWARNING: This test requires PkiExpress, version greater than 1.11\n' +
+                    'Also, you should have the Pierre de Fermat certificate installed in your machine\n'
+			);
+			try {
+				// Mount a default request using default PKIBrazilParameters
+				let request = mountOrderRequest(true);
 
-                const order = await ampliaClient.createOrder(request);
-                // Get an instance of the Key Generator class, responsible for generate
-                // a private key and the corresponding CSR.
-                const keyGenerator = new KeyGenerator({
-                    keySize: "2048",
-                    genCsr: true,
-                });
+				const order = await ampliaClient.createOrder(request);
+				// Get an instance of the Key Generator class, responsible for generate
+				// a private key and the corresponding CSR.
+				const keyGenerator = new KeyGenerator({
+					keySize: '2048',
+					genCsr: true,
+				});
 
-                // Generate private key and CSR.
-                let genKey = await keyGenerator.generate();
+				// Generate private key and CSR.
+				let genKey = await keyGenerator.generate();
 
-                // Call Amplia in order to issue the certificate referred by the
-                // created order's id.
+				// Call Amplia in order to issue the certificate referred by the
+				// created order's id.
 
-                const cert = await ampliaClient.issueCertificate(
-                    order.getId(),
-                    genKey.getCsr(),
-                    "PC"
-                );
+				const cert = await ampliaClient.issueCertificate(
+					order.getId(),
+					genKey.getCsr(),
+					'PC'
+				);
 
-                // Retrieve the order
-                const retrievedOrder = await ampliaClient.getOrder(order.id);
-                // checks if the order has been issued
-                expect(retrievedOrder.getStatus()).toBe(OrderStatus.ISSUED);
+				// Retrieve the order
+				const retrievedOrder = await ampliaClient.getOrder(order.id);
+				// checks if the order has been issued
+				expect(retrievedOrder.getStatus()).toBe(OrderStatus.ISSUED);
 
-                // redundant tests
-                expect(retrievedOrder.id).toBe(order.id);
-                expect(retrievedOrder.caId).toBe(order.caId);
-                expect(retrievedOrder.kind).toBe(order.kind);
-                expect(retrievedOrder.alias).toBe(order.alias);
-                expect(retrievedOrder.validityEnd).toBe(order.validityEnd);
-                expect(retrievedOrder.name).toBe(order.name);
-                // end redundant tests
+				// redundant tests
+				expect(retrievedOrder.id).toBe(order.id);
+				expect(retrievedOrder.caId).toBe(order.caId);
+				expect(retrievedOrder.kind).toBe(order.kind);
+				expect(retrievedOrder.alias).toBe(order.alias);
+				expect(retrievedOrder.validityEnd).toBe(order.validityEnd);
+				expect(retrievedOrder.name).toBe(order.name);
+				// end redundant tests
 
-                const retrievedCert = await ampliaClient.getCertificate(
-                    retrievedOrder.getCertificateId()
-                );
-                // certificate tests
-                expect(retrievedCert.id).toBe(cert.id);
-                expect(retrievedCert.certificateId).toBe(cert.certificateId);
-                expect(retrievedCert.caId).toBe(cert.caId);
-                expect(retrievedCert.alias).toBe(cert.alias);
-                expect(retrievedCert.subjectName).toEqual(cert.subjectName);
-                expect(retrievedCert.issuerName).toEqual(cert.issuerName);
-                expect(retrievedCert.serialNumber).toBe(cert.serialNumber);
-                expect(retrievedCert.validityStart).toBe(cert.validityStart);
-                expect(retrievedCert.validityEnd).toBe(cert.validityEnd);
-                expect(retrievedCert.crlDistributionPoints).toEqual(
-                    cert.crlDistributionPoints
-                );
-                expect(retrievedCert.ocspUris).toEqual(cert.ocspUris);
-                expect(retrievedCert.content).toBe(cert.content);
-                expect(retrievedCert.kind).toBe(cert.kind);
-                expect(retrievedCert.format).toBe(cert.format);
+				const retrievedCert = await ampliaClient.getCertificate(
+					retrievedOrder.getCertificateId()
+				);
+				// certificate tests
+				expect(retrievedCert.id).toBe(cert.id);
+				expect(retrievedCert.certificateId).toBe(cert.certificateId);
+				expect(retrievedCert.caId).toBe(cert.caId);
+				expect(retrievedCert.alias).toBe(cert.alias);
+				expect(retrievedCert.subjectName).toEqual(cert.subjectName);
+				expect(retrievedCert.issuerName).toEqual(cert.issuerName);
+				expect(retrievedCert.serialNumber).toBe(cert.serialNumber);
+				expect(retrievedCert.validityStart).toBe(cert.validityStart);
+				expect(retrievedCert.validityEnd).toBe(cert.validityEnd);
+				expect(retrievedCert.crlDistributionPoints).toEqual(
+					cert.crlDistributionPoints
+				);
+				expect(retrievedCert.ocspUris).toEqual(cert.ocspUris);
+				expect(retrievedCert.content).toBe(cert.content);
+				expect(retrievedCert.kind).toBe(cert.kind);
+				expect(retrievedCert.format).toBe(cert.format);
 
-                // end certificate tests
-                try {
-                    // Delete the order after all tests
-                    await ampliaClient.deleteOrder(order.id);
-                } catch (e) {
-                    // only this time, the order will be fulfilled and it will be impossible to remove
-                    expect(e.name).toMatch("AmpliaError");
-                    expect(e.code).toBe("OrderAlreadyFulfilled");
-                }
+				// end certificate tests
+				try {
+					// Delete the order after all tests
+					await ampliaClient.deleteOrder(order.id);
+				} catch (e) {
+					// only this time, the order will be fulfilled and it will be impossible to remove
+					expect(e.name).toMatch('AmpliaError');
+					expect(e.code).toBe('OrderAlreadyFulfilled');
+				}
 
-                // we can, however, revoke the previously generated certificate
-                await ampliaClient.revokeCertificate(cert.id);
-                // test if the date of certificate has been revoked is less than the now date
-                const certAfterRevocation = await ampliaClient.getCertificate(
-                    cert.id
-                );
-                
-                const dateRevoked = new Date(certAfterRevocation.dateRevoked);
-                expect(dateRevoked.getTime()).toBeLessThanOrEqual(
-                    new Date().getTime()
-                );
-            } catch (error) {
-                console.log(error);
-            }
-        },
-        LONG_TIMEOUT
-    );
-    // end Test Case #3 for Amplia Node Client
+				// we can, however, revoke the previously generated certificate
+				await ampliaClient.revokeCertificate(cert.id);
+				// test if the date of certificate has been revoked is less than the now date
+				const certAfterRevocation = await ampliaClient.getCertificate(
+					cert.id
+				);
+
+				const dateRevoked = new Date(certAfterRevocation.dateRevoked);
+				expect(dateRevoked.getTime()).toBeLessThanOrEqual(
+					new Date().getTime()
+				);
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		LONG_TIMEOUT
+	);
+	// end Test Case #3 for Amplia Node Client
 });
